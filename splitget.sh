9c8dbc22e97b5caf6f9480d1b4a6ecc7c2350839
Hindol Adhya <hindol.adhya@gmail.com>
2012-05-04 01:24:35 +0530

    First working prototype

--- splitget.sh
+++ splitget.sh
@@ -1,5 +1,5 @@
-# splitget.sh (Multipart file downloader)
 #!/bin/bash
+# splitget.sh (Multipart file downloader)
 
 # Command line arguments
 # -i < url to download >
@@ -41,46 +41,41 @@ done
 : ${chunk_size=157286400}
 : ${parallel="off"}
 
-if [ -z "$output" ]
-	then
+if [ -z "$output" ]; then
 	# Detect filename from URI
 	output=$( echo $input | perl -MURI -le 'chomp($url = <>); $filename = (URI->new($url)->path_segments)[-1]; print $filename' )
 fi
 
 # Try to detect file size by inspecting HTTP header
-detected_size=$( curl -I $input 2>|/dev/null | awk '/^Content-Length:/ { print $2 }' )
+header=$( curl -I $input 2> /dev/null )
+detected_size=$( echo "$header" | awk '/^Content-Length:/ { print $2 }' | tr -d '\r' )
 
 # If size can be detected from header, override size input
-if [[ $detected_size != "" ]]
-	then
+if [ "$detected_size" != "" ]; then
 	size=$detected_size
 fi
 
-if [[ $size != "" ]]
-	then
-	start=0
-	end=$(( chunk_size - 1 ))
-	counter=1
-	while [ $start -le $size ]
-	do
-		if [ $end -gt $size ]
-			then
-			end=$size
-		fi
-
-		curl --range $start-$end -o $output.part$counter $input&
-
-		start+=$chunk_size
-		end+=$chunk_size
-		counter+=1
-	done
-
+if [ "$size" != "" ]; then
 	# Kill background jobs on exit
 	trap 'kill 0' SIGINT SIGTERM EXIT
+
+	chunks=$(( $size / $chunk_size + 1 ))
+	# Loop one time less than the number of chunks
+	for (( i=1; i<chunks; i++ )); do
+		start=$(( (i - 1) * chunk_size ))
+		echo $start
+		end=$(( start + chunk_size - 1 ))
+		echo $end
+		curl --range $start-$end -o $output.part$i $input&
+	done
+
+	start=$(( (chunks - 1) * chunk_size ))
+	echo $start
+	curl --range $start- -o $output.part$i $input&
 
 	wait # Wait till all parts are downloaded
 
 	# Merge parts into single file
-	cat $output.part? > $output
-	rm $output.part?
+	cat $output.part* > $output
+	rm $output.part*
 fi
