df259dd6453753b43557f0d19bf156f1abb27932
Hindol Adhya <hindol.adhya@gmail.com>
2012-05-05 00:39:39 +0530

    Improved code readability

--- splitget.sh
+++ splitget.sh
@@ -9,68 +9,79 @@
 # -l < maximum chunk size in bytes >
 # -m ? Download file in multiple chunks parallely
 
-while getopts "i:o:p:s:l:m" OPTION; do
-	case "$OPTION" in
-		i)
-			input="$OPTARG"
-			;;
-		o)
-			output="$OPTARG"
-			;;
-		p)
-			proxy="$OPTARG"
-			;;
-		s)
-			size="$OPTARG"
-			;;
-		l)
-			chunk_size="$OPTARG"
-			;;
-		m)
-			parallel="on"
-			;;
-		*)
-			echo "Unrecognized option!"
-			echo "$usage"
-			;;
-	esac
-done
+# Global variables used throughout this script
+# $input     	The URL to download
+# $output    	Output filename
+# $proxy     	The proxy to be used for making all connections
+# $size      	File size of the target URL
+# $chunk_size	How large each chunk should be (default 150MB)
+# $parallel  	Download chunks parallely (if set) / sequentially (if not set)?
 
-# Set default values
-: ${proxy=$http_proxy}
-: ${chunk_size=157286400}
-: ${parallel="off"}
+function readArgs() {
+	while getopts "i:o:p:s:l:m" OPTION; do
+		case "$OPTION" in
+			i)
+				input="$OPTARG"
+				;;
+			o)
+				output="$OPTARG"
+				;;
+			p)
+				proxy="$OPTARG"
+				;;
+			s)
+				size="$OPTARG"
+				;;
+			l)
+				chunk_size="$OPTARG"
+				;;
+			m)
+				parallel="on"
+				;;
+			*)
+				echo "Unrecognized option!"
+				echo "$usage"
+				;;
+		esac
+	done
+}
 
-if [ -z "$output" ]; then
-	# Detect filename from URI
-	output=$( echo $input | perl -MURI -le 'chomp($url = <>); $filename = (URI->new($url)->path_segments)[-1]; print $filename' )
-fi
+function setDefaults() {
+	: ${proxy=$http_proxy}
+	: ${chunk_size=157286400}
+	: ${parallel="off"}
 
-# Try to detect file size by inspecting HTTP header
-header=$( curl -I $input 2> /dev/null )
-detected_size=$( echo "$header" | awk '/^Content-Length:/ { print $2 }' | tr -d '\r' )
+	if [ -z "$output" ]; then
+		# Detect filename from URI
+		output=$( echo $input | perl -MURI -le 'chomp($url = <>); $filename = (URI->new($url)->path_segments)[-1]; print $filename' )
+	fi
 
-# If size can be detected from header, override size input
-if [ "$detected_size" != "" ]; then
-	size=$detected_size
-fi
+	# Try to detect file size by inspecting HTTP header
+	header=$( curl -I $input 2> /dev/null )
+	detected_size=$( echo "$header" | awk '/^Content-Length:/ { gsub("\015", "", $2); print $2 }' )
+
+	# If size can be detected from header, override size input
+	if [ "$detected_size" != "" ]; then
+		size=$detected_size
+	fi
+}
+
+readArgs "$@"	# Read command line inputs
+setDefaults  	# Set defaults for unspecified inputs
 
 if [ "$size" != "" ]; then
 	# Kill background jobs on exit
 	trap 'kill 0' SIGINT SIGTERM EXIT
 
-	chunks=$(( $size / $chunk_size + 1 ))
+	chunks=$(( ($size - 1) / $chunk_size + 1 ))
 	# Loop one time less than the number of chunks
 	for (( i=1; i<chunks; i++ )); do
 		start=$(( (i - 1) * chunk_size ))
-		echo $start
 		end=$(( start + chunk_size - 1 ))
-		echo $end
 		curl --range $start-$end -o $output.part$i $input&
 	done
 
 	start=$(( (chunks - 1) * chunk_size ))
-	echo $start
 	curl --range $start- -o $output.part$i $input&
 
 	wait # Wait till all parts are downloaded
