27207c46c4ca517eb2eadba3fc27cb6605812a94
Hindol Adhya <hindol.adhya@gmail.com>
2012-05-05 10:45:07 +0530

    Refactored code

--- splitget.sh
+++ splitget.sh
@@ -40,7 +40,7 @@ function readArgs() {
 				;;
 			*)
 				echo "Unrecognized option!"
-				echo "$usage"
+				echo "${usage}"
 				;;
 		esac
 	done
@@ -51,42 +51,63 @@ function setDefaults() {
 	: ${chunk_size=157286400}
 	: ${parallel="off"}
 
-	if [ -z "$output" ]; then
+	if [[ -z "${output}" ]]; then
 		# Detect filename from URI
-		output=$( echo $input | perl -MURI -le 'chomp($url = <>); $filename = (URI->new($url)->path_segments)[-1]; print $filename' )
+		output=$( echo ${input} | perl -MURI -le 'chomp($url = <>); ${filename} = (URI->new(${url})->path_segments)[-1]; print ${filename}' )
 	fi
 
 	# Try to detect file size by inspecting HTTP header
-	header=$( curl -I $input 2> /dev/null )
-	detected_size=$( echo "$header" | awk '/^Content-Length:/ { gsub("\015", "", $2); print $2 }' )
+	header=$( curl -I ${input} 2> /dev/null )
+	detected_size=$( echo "${header}" | awk '/^Content-Length:/ { gsub("\015", "", $2); print $2 }' )
 
 	# If size can be detected from header, override size input
-	if [ "$detected_size" != "" ]; then
+	if [[ "${detected_size}" != "" ]]; then
 		size=$detected_size
 	fi
+}
+
+function cleanUp() {
+	# Clean up temporary files
+	for file in $output.part*; do
+		if [[ -e ${file} ]]; then
+			rm ${file}
+		fi
+	done
+}
+
+function abort() {
+	kill $(jobs -pr)
+	cleanUp # Cleanup temporary files
+
+	# Clean output file if exists
+	if [[ -e ${output} ]]; then
+		rm ${output}
+	fi
+
+	exit
 }
 
 readArgs "$@"	# Read command line inputs
 setDefaults  	# Set defaults for unspecified inputs
 
-if [ "$size" != "" ]; then
-	# Kill background jobs on exit
-	trap 'kill 0' SIGINT SIGTERM EXIT
+if [[ "${input}" != "" && "${size}" != "" ]]; then
+	# Kill background jobs when interrupted
+	trap 'cleanUp' EXIT
+	trap 'abort' SIGINT
 
-	chunks=$(( ($size - 1) / $chunk_size + 1 ))
+	chunks=$(( (size - 1) / chunk_size + 1 ))
 	# Loop one time less than the number of chunks
 	for (( i=1; i<chunks; i++ )); do
 		start=$(( (i - 1) * chunk_size ))
 		end=$(( start + chunk_size - 1 ))
-		curl --range $start-$end -o $output.part$i $input&
+		curl --range ${start}-${end} -o ${output}.part${i} ${input} &
 	done
 
 	start=$(( (chunks - 1) * chunk_size ))
-	curl --range $start- -o $output.part$i $input&
+	curl --range ${start}- -o ${output}.part${i} ${input} &
 
 	wait # Wait till all parts are downloaded
 
 	# Merge parts into single file
-	cat $output.part* > $output
-	rm $output.part*
+	cat ${output}.part* > ${output}
 fi
