d23cb5c930ed0542e843e17d98f02c4742f052e8
Hindol Adhya <hindol.adhya@gmail.com>
2012-05-05 10:37:14 +0530

    Removed test-splitget.sh from repo

--- test-splitget.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# ./splitget.sh -i http://sublime.wbond.net/Package%20Control.sublime-package
-./splitget.sh -l 25000000 -i http://downloads.zend.com/pdt/helios/zend-eclipse-php-helios-linux-gtk-x86_64.tar.gz
